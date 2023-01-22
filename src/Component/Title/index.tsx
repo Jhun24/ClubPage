791f27a8031eefa4d287531803029b905416eb02
Jhun24 <jjanghuni102@naver.com>
2023-01-22 14:22:16 +0900

    setting: setting repository

---
+++ src/Component/Title/index.tsx
@@ -0,0 +1,14 @@
+
+import styles from './Title.module.css';
+
+interface TitleProps {
+
+}
+
+export default function Title({}: TitleProps){
+    return(
+        <div className={styles.title_box}>
+            <img src={""}/>
+        </div>
+    );    
+}
